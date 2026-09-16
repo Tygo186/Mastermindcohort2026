6ad254dd3b212e3e8115c7839731b3e714d21435
Tygo186 <1225363@student.roc-nijmegen.nl>
2026-09-16 12:01:22 +0200

    commit
    added variables for my codes

--- Mastermind/masterMind.java
+++ Mastermind/masterMind.java
@@ -1,10 +1,129 @@
 package Mastermind;
 
+import java.util.Scanner;
+
 public class masterMind {
 	
 	public static void main(String[] args) {
+		Scanner Scanner = new Scanner(System.in);
 		
 		System.out.println("Hoi Mastermind 2026");
+		
+		String rodePin = "Rood";
+		String blauwePin = "Blauw";
+		String gelePin = "Geel";
+		String groenePin = "Groen";
+		String oranjePin = "Oranje";
+		String paarsePin = "Paars";
+		
+		String secretcode = "paarsePin, rodePin, blauwePin, groenePin";
+		  
+		String zwartePin = "Zwart";
+		String wittePin = "Wit";
+		String legePin = "Leeg";
+		
+		String vakje1Rij1 = "1-1";
+		String vakje2Rij1 = "1-2";
+		String vakje3Rij1 = "1-3";
+		String vakje4Rij1 = "1-4";
+		
+		String vakje1Rij2 = "2-1";
+		String vakje2Rij2 = "2-2";
+		String vakje3Rij2 = "2-3";
+		String vakje4Rij2 = "2-4";
+		
+		String vakje1Rij3 = "3-1";
+		String vakje2Rij3 = "3-2";
+		String vakje3Rij3 = "3-3";
+		String vakje4Rij3 = "3-4";
+		
+		String vakje1Rij4 = "4-1";
+		String vakje2Rij4 = "4-2";
+		String vakje3Rij4 = "4-3";
+		String vakje4Rij4 = "4-4";
+		
+		String vakje1Rij5 = "5-1";
+		String vakje2Rij5 = "5-2";
+		String vakje3Rij5 = "5-3";
+		String vakje4Rij5 = "5-4";
+		
+		String vakje1Rij6 = "6-1";
+		String vakje2Rij6 = "6-2";
+		String vakje3Rij6 = "6-3";
+		String vakje4Rij6 = "6-4";
+		
+		String vakje1Rij7 = "7-1";
+		String vakje2Rij7 = "7-2";
+		String vakje3Rij7 = "7-3";
+		String vakje4Rij7 = "7-4";
+		
+		String vakje1Rij8 = "8-1";
+		String vakje2Rij8 = "8-2";
+		String vakje3Rij8 = "8-3";
+		String vakje4Rij8 = "8-4";
+		
+		String vakje1Rij9 = "9-1";
+		String vakje2Rij9 = "9-2";
+		String vakje3Rij9 = "9-3";
+		String vakje4Rij9 = "9-4";
+		
+		String vakje1Rij10 = "10-1";
+		String vakje2Rij10 = "10-2";
+		String vakje3Rij10 = "10-3";
+		String vakje4Rij10 = "10-4";
+		
+		String makervakje1Rij1;
+		String makervakje1Rij1;
+		String makervakje2Rij1;
+		String makervakje3Rij1;
+		String makervakje4Rij1;
+		
+		String makervakje1Rij2;
+		String makervakje2Rij2;
+		String makervakje3Rij2;
+		String makervakje4Rij2;
+		
+		String makervakje1Rij3;
+		String makervakje2Rij3
+		String makervakje3Rij3;
+		String makervakje4Rij3;
+		
+		String makervakje1Rij4;
+		String makervakje2Rij4;
+		String makervakje3Rij4;
+		String makervakje4Rij4;
+		
+		String makervakje1Rij5;
+		String makervakje2Rij5;
+		String makervakje3Rij5;
+		String makervakje4Rij5;
+		
+		String makervakje1Rij6;
+		String makervakje2Rij6;
+		String makervakje3Rij6;
+		String makervakje4Rij6;
+		
+		String makervakje1Rij7;
+		String makervakje2Rij7;
+		String makervakje3Rij7;
+		String makervakje4Rij7;
+		
+		String makervakje1Rij8;
+		String makervakje2Rij8;
+		String makervakje3Rij8;
+		String makervakje4Rij8;
+		
+		String makervakje1Rij9;
+		String makervakje2Rij9;
+		String makervakje3Rij9;
+		String makervakje4Rij9;
+		
+		String makervakje1Rij10;
+		String makervakje2Rij10;
+		String makervakje3Rij10;
+		String makervakje4Rij10;
+		
+		
+	
 	}
-
 }
